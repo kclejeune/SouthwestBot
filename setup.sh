828f75ee4522b7cd1675e963e140ac71af6e9544
Kennan LeJeune <kennan@case.edu>
2018-06-05 02:31:51 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# clone the repostiory
-git clone https://github.com/kclejeune/SouthwestBot.git
-
 # check if homebrew is installed and install if not
 if [ ! -e /usr/local/bin/brew ]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+# clone the repostiory
+git clone https://github.com/kclejeune/SouthwestBot.git
 
 # install necessary dependencies
 brew cask install chromedriver
